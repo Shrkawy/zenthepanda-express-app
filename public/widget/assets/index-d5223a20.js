14d5fbb93b104d6c41500d81a4b37bdb58296bcc
Mahmoud Alshrkawy <mahmoud.alshrkawy@gmail.com>
2024-07-03 19:23:11 +0300

    Update widget script to use latest version

--- public/widget/assets/index-d5223a20.js
+++ public/widget/assets/index-d5223a20.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as E,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-9ac98e00.js";import{u as M,b3 as Ur}from"./index-5f3daad4.js";/**
+import{n as fe,s as Ke,T as E,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-6ee86f18.js";import{u as M,b3 as Ur}from"./index-7de4650e.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
