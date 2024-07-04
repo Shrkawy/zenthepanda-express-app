ff6575a57d0f6bfd0325f816be49660d84321d83
Mahmoud Alshrkawy <mahmoud.alshrkawy@gmail.com>
2024-07-04 15:31:54 +0300

    chore: Update widget script to use latest version

--- public/widget/assets/index-92df2c00.js
+++ public/widget/assets/index-92df2c00.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as E,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-85e56494.js";import{u as M,b3 as Ur}from"./index-f89143ae.js";/**
+import{n as fe,s as Ke,T as E,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-be233466.js";import{u as M,b3 as Ur}from"./index-cf655d5a.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
