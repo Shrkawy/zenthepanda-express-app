653677c43e2377eab2c45cd846a74333fb1d611a
Mahmoud Alshrkawy <mahmoud.alshrkawy@gmail.com>
2024-07-09 18:00:09 +0300

    chore: Update staking.html and style.css for offchain lookup feature

--- public/staking/assets/index-B11K-lN_.js
+++ public/staking/assets/index-B11K-lN_.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as E,t as P,a as g,o as Y,R as Ur,p as H,y as ae}from"./index-Day4KH6g.js";import{o as R,b0 as jr}from"./index-Cxd4yzU1.js";/**
+import{n as fe,s as Ke,T as E,t as P,a as g,o as Y,R as Ur,p as H,y as ae}from"./index-Bn-PRbdq.js";import{o as R,b0 as jr}from"./index-CqgOwdM6.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
